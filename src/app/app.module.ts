f9dee1c68bd87ab7822605c6fcd6131e4178c30e
jobin <jobinjose.in@gmail.com>
2022-11-24 14:45:15 +0530

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentEntryComponent } from './student-entry/student-entry.component';
+
+const myRoutes:Routes = [
+  {
+    path:"",
+    component:StudentEntryComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -12,7 +20,8 @@ import { StudentEntryComponent } from './student-entry/student-entry.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

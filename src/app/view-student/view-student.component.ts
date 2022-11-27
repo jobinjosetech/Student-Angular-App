0e2c3ad76a0898d0a87cb66a8cec22c65a3fd04b
jobin <jobinjose.in@gmail.com>
2022-11-27 23:58:04 +0530

    view student

--- src/app/view-student/view-student.component.ts
+++ src/app/view-student/view-student.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-add-student',
+  selector: 'app-view-student',
   templateUrl: './view-student.component.html',
   styleUrls: ['./view-student.component.css']
 })
